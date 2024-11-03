1a0c07f529d0c011a01368f8624106c7c383d5d2
Tchelo2266 <tchelo2266@outlook.com>
2024-11-03 16:17:44 -0300

    v1.0.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: [
+    'bootstrap/dist/css/bootstrap.css',
+    '@/assets/css/main.css'
+  ],
+  components: true,
 })
